c03119e988a5c0aae26e35651c4d76640ed861ed
JonyPlo <joonyyplo@gmail.com>
2023-05-23 00:51:59 -0300

    se realizo la logica de practica para el useReducer

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -7,7 +7,8 @@ export const MultipleCustomHooks = () => {
     `http://localhost:3000/breakingbad?quote_id=${counter}`
   );
   const { allQuotes } = useFetchAllQuotes('http://localhost:3000/breakingbad');
-  const { quote, author } = !!data && data[0]; //Aqui aplicamos doble negacion (!!) se hace esto porque "data" al principio es null por lo tanto no se puede desestructurar algo de null, asi que al aplicar la primera negacion, osea !data, transformamos el valor de null a true, y con la segunda negacion !!data convertimos el valor de true a false, por lo tanto de esta forma estamos haciendo que solo se desestructure cuando data tenga datos
+  const { quote, author } = !!data && data[0]; //Aqui aplicamos doble negacion (!!) se hace esto porque "data" al principio es null por lo tanto no se puede desestructurar algo de null, asi que al aplicar la primera negacion, osea !data, transformamos el valor de null a true, y con la segunda negacion !!data convertimos el valor de true a false, por lo tanto de esta forma estamos haciendo que solo se desestructure cuando data tenga datos.
+  // Otra opcion para convertir el data en un Booleano seria con la funcion de javascript Boolean(data), esto convertira al data con el valor null en un booleano, en este caso como el valor es null entonces el Boolean() lo convertira en false hasta que tenga datos y lo convertira a true
 
   const disabledButtonNext = isLoading || counter === allQuotes.length;
   const disabledButtonPrevious = isLoading || counter === 1;
