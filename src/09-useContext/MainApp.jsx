5f87c509e756f813aacd9b421012d38a754ae209
JonyPlo <joonyyplo@gmail.com>
2023-05-26 06:04:33 -0300

    se realizaron ajustes de formato de codigo

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,5 +1,5 @@
-import { Outlet } from 'react-router-dom';
-import { Navbar } from './';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar, HomePage, AboutPage, LoginPage } from './';
 import { UserProvider } from './context/UserProvider';
 
 export const MainApp = () => {
@@ -7,9 +7,12 @@ export const MainApp = () => {
     <UserProvider>
       <Navbar />
       <hr />
-
-      {/* Este componente llamado Outlet se encarga de mostrar las rutas hijas que estan dentro de una ruta padre, en este caso MainApp es el componente padre que tendra todas las rutas hijas asi que debe ir el componente Outlet al final, de lo contrario no se mostraran los componentes que esten en las rutas hijas de MainApp */}
-      <Outlet />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='*' element={<Navigate to='about' />} />
+      </Routes>
     </UserProvider>
   );
 };
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-// const user = {
-//   id: 123,
-//   name: 'Jony',
-//   email: 'joonyyplo@gmail.com',
-// };
-
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState();
 
   return (
-    // <UserContext.Provider value={{ hola: 'Mundo', user }}>
     <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
